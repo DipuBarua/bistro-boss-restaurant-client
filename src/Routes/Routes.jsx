fa1cb15fdae10f3ac395019c2fb1de7a9247c95a
DipuBarua <you@example.com>
2023-11-13 23:36:06 +0600

    updated banner, footer, navbar

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Layout/Root";
-import Home from "../pages/Home/Home";
+import Home from "../pages/Home/Home/Home";
 
 const router = createBrowserRouter([
     {
